e72fe66f144ae6895828ae3a6a18557c958c0b75
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-09 15:17:34 +0100

    Update pdf_encrypted_check.py

--- pdf_encrypted_check.py
+++ pdf_encrypted_check.py
@@ -1,11 +1,13 @@
 import PyPDF2
 import os
+import sys
 
 def check():
-  for i in os.listdir(os.curdir):
+  for i in sys.argv[1::]:
       if i.endswith('.pdf'):
 
           PDF = PyPDF2.PdfReader(i)
           print(PDF.is_encrypted)
-      
-check()
+
+      else:
+          print(f'{i} ---- is not a pdf-file ----')

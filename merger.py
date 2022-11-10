74d9f5ef602add79b95489b1f3f92a393474f8c4
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-10 19:10:06 +0100

    Update merger.py

--- merger.py
+++ merger.py
@@ -2,20 +2,22 @@ import PyPDF2
 import os
 import sys
 
-# initialize PdfMerger-Object.
-# iterate with for loop over sys.argv-list starting at element 1 not at element 0.
-# only .pdf-files getting appended to the PdfMerger-Object.
-# write files wchich are stored in the PdfMerger-Object to new files with the write()-Function.
+# initialize PdfMerger-Object. Create aan empty list called lists
+# while loop to insert as many pdf as wanted
+# pdf-file is getting appended to empty list
+# iterate with for loop over list(lists) to insert pdf file to PDF(PdfMerger)-Object
+# create new merged file with write()-methode
+# close PDF(PdfMerger)-Object with close()-methode
 
 def extract():
     PDF = PyPDF2.PdfMerger()
     lists = []
     while True:
         s = input('Drag and Drop File here (Press just Enter when you inserted desired files): ')
-        lists.append(s)
+        lists.append(s[:-1])
         if s == True:
             s = input('Drag and Drop File here (Press just Enter when you inserted desired files): ')
-            lists.append(s)
+            lists.append(s[:-1])
         elif s == '':
             break
             

d3aea51ad75921dc6c379bfb32b3989db75be758
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-10 13:25:15 +0100

    Update merger.py

--- merger.py
+++ merger.py
@@ -2,6 +2,10 @@ import PyPDF2
 import os
 import sys
 
+# initialize PdfMerger-Object.
+# iterate with for loop over sys.argv-list starting at element 1 not at element 0.
+# only .pdf-files getting appended to the PdfMerger-Object.
+# write files wchich are stored in the PdfMerger-Object to new files with the write()-Function.
 
 def merge():
     PDF = PyPDF2.PdfMerger()

8f7e68ef154b0296d064130100772637d702ace1
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-10 13:25:43 +0100

    Update pdf_encrypted_check.py

--- pdf_encrypted_check.py
+++ pdf_encrypted_check.py
@@ -3,10 +3,10 @@ import os
 import sys
 
 
-#itereate over sys.argv-list starting at position 1
-#only files with the .pdf file-extensions will be read by the PdfReader
-#files with different file-extensions won't be read. Instead there is a ---- is not a pdf-file ---- output
-#encrypted > true // not encrypted > false
+# itereate over sys.argv-list starting at position 1
+# only files with the .pdf file-extensions will be read by the PdfReader
+# files with different file-extensions won't be read. Instead there is a ---- is not a pdf-file ---- output
+# encrypted > true // not encrypted > false
 
 def check():
   for i in sys.argv[1::]:

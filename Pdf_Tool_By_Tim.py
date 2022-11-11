acf6fdfcd13fe13fe5bd12e6f3f4af730cf20152
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-11 16:32:58 +0100

    Update Pdf_Tool_By_Tim.py

--- Pdf_Tool_By_Tim.py
+++ Pdf_Tool_By_Tim.py
@@ -1,1 +1,40 @@
-#test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def intro():
+    introduction = ['PDF-TOOL by TIM\n\n', '1 --- Pdf-Dateien zusammenführen\n', '2 --- Pdf-Dateien enkrypten\n', '--\n', '--\n', 'Geben Sie die Nummer ein um eine der angeführeten Optionen zu wählen']
+
+    for i in introduction:
+        print(i)
+    
+    s = int(input('>>> '))
+    
+    if s == 1:
+        print('Function1')
+    elif s == 2:
+        print('Function2')
+    elif s == 3:
+        print('Function3')
+    elif s == 3:
+        print('Function4')
+    else:
+        print('Ungültige Eingabe...')
+        intro()
+    
+    
+        
+
+
+        
+intro()

7de691f9f887c849c6cc7837682bde57f12b3659
TimGeNW <117636264+TimGeNW@users.noreply.github.com>
2022-11-11 16:35:29 +0100

    Update Pdf_Tool_By_Tim.py

--- Pdf_Tool_By_Tim.py
+++ Pdf_Tool_By_Tim.py
@@ -1,3 +1,35 @@
+import PyPDF2
+import os
+import sys
+
+# initialize PdfMerger-Object. Create aan empty list called lists
+# while loop to insert as many pdf as wanted
+# pdf-file is getting appended to empty list
+# iterate with for loop over list(lists) to insert pdf file to PDF(PdfMerger)-Object
+# create new merged file with write()-methode
+# close PDF(PdfMerger)-Object with close()-methode
+
+def extract():
+    PDF = PyPDF2.PdfMerger()
+    lists = []
+    while True:
+        s = input('Drag and Drop File here (Press just Enter when you inserted desired files): ')
+        lists.append(s[:-1])
+        if s == True:
+            s = input('Drag and Drop File here (Press just Enter when you inserted desired files): ')
+            lists.append(s[:-1])
+        elif s == '':
+            break
+            
+    for i in lists:
+        if i.endswith('.pdf'):
+            PDF.append(i)
+
+
+            
+    PDF.write('new_merged.pdf')
+    PDF.close()
+
 
 
 
@@ -21,7 +53,7 @@ def intro():
     s = int(input('>>> '))
     
     if s == 1:
-        print('Function1')
+        extract()
     elif s == 2:
         print('Function2')
     elif s == 3:
